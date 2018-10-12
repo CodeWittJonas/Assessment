b7a5721e89f5cdde397675ce7709bd476c18f067
j94wittwer <j94wittwer@gmail.com>
2018-10-12 22:13:10 +0200

    implemented E4/task_4

--- Informatik/E4/task_4.py
+++ Informatik/E4/task_4.py
@@ -17,8 +17,28 @@ def shuffle_letters(letters):
 
 
 # ======== You can define the encrypt and decrypt function here. Do not write any code other than your solution here! ================
+def encrypt(plain_text, key):
+    if len(key) == 26:
+        cipher_text = ""
+        plain_text = plain_text.lower()
+        for char in plain_text:
+            index = alphabet.index(char)
+            cipher_text = cipher_text + key[index]
+        return cipher_text
+    else:
+        return ""
 
 
+def decrypt(cipher_text, key):
+    if len(key) == 26:
+        plain_text = ""
+        cipher_text = cipher_text.lower()
+        for char in cipher_text:
+            index = key.index(char)
+            plain_text = plain_text + alphabet[index]
+        return plain_text
+    else:
+        return ""
 
 
 
@@ -30,22 +50,22 @@ if __name__ == '__main__':
     """
         Keys to play around with. You can generate new ones if you want, using the 'shuffle_letters' function!
         Also try calling print_cipher with one of these keys to see how the mapping works.
-        
-        key_1 = 'wfrjucxmeziqpagbsnlkytvhod'
-    
-        key_2 = 'vzfpbncyrosdwjqumhikxlegat'
-    
-        key_3 = 'gcbmkezpsiaxdqvhonjftulyrw'
-        
-        print_cipher(key_1)
-    
-        plain_text = input('Please enter text to encrypt: ').lower() # input: 'hello'
-    
-        cipher = encrypt(plain_text, key) # cipher = 'muqqg'
-    
-        print(f'Encrypted text:\t "{cipher}"') # prints Encrypted text:     "muqqg"
-    
-        plain = decrypt(cipher, key) # plain = 'hello'
-    
-        print(f'Decrypted ciphertext:\t "{plain}"') # prints Decrypted ciphertext:   "hello"
-        """
+    """
+
+    key_1 = 'wfrjucxmeziqpagbsnlkytvhod'
+
+    key_2 = 'vzfpbncyrosdwjqumhikxlegat'
+
+    key_3 = 'gcbmkezpsiaxdqvhonjftulyrw'
+
+
+    plain_text = "hello"
+
+    cipher = encrypt(plain_text, key_2)  # cipher = 'muqqg'
+
+    print(f'Encrypted text:\t "{cipher}"')  # prints Encrypted text:     "muqqg"
+
+    plain = decrypt(cipher, key_2)  # plain = 'hello'
+
+    print(f'Decrypted ciphertext:\t "{plain}"')  # prints Decrypted ciphertext:   "hello"
+
