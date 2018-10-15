5f81093f3d9c219cb79dae49e22124963c205935
j94wittwer <j94wittwer@gmail.com>
2018-10-15 14:36:58 +0200

    Exercise 4 complete

--- Informatik/E4/task_3.py
+++ Informatik/E4/task_3.py
@@ -4,7 +4,20 @@ def tokenize(text):
 
 
 # ======== You can define the search_text function here. Do not write any code other than your solution here! ================
+def search_text(filename, query):
+    content = ""
+    num_of_occurences = 0
+    with open(filename, 'r') as file:
+        for line in file.readlines():
+            content = content + line
+        content = content.lower()
+        content = tokenize(content)
+        file.close()
+    for word in content:
+        if word == query:
+            num_of_occurences += 1
 
+    return num_of_occurences
 
 
 
@@ -20,3 +33,5 @@ if __name__ == '__main__':
 
     print(f'Found {occurrences} occurrences of {query} in text.')
     """
+
+    print(search_text('lorem-ipsum.txt', "lorem"))
