6dec400586dcb8ebdf3083f0882f829750fb37d6
j94wittwer <j94wittwer@gmail.com>
2018-11-15 13:58:16 +0100

    implemented task_2.py

--- Informatik/E8/task_1.py
+++ Informatik/E8/task_1.py
@@ -1,3 +1,69 @@
+
+
+# ========= Your classes ==========
+class Aircraft:
+
+    def __init__(self, number_of_passengers, name):
+        self._number_of_passengers = number_of_passengers
+        self._name = name
+
+    def calculate_amount_of_fuel(self, km):
+        pass
+
+    def get_number_of_passengers(self):
+        return self._number_of_passengers
+
+    def get_name(self):
+        return self._name
+
+
+class IntercontinentalAircraft(Aircraft):
+
+    def __init__(self, number_of_passengers, name, cargo_hold):
+        super().__init__(number_of_passengers, name)
+        self.__cargo_hold = cargo_hold
+        self.manifest = "Intercontinental flight {}: passenger count {}, cargo load {}".format(self.get_name(), self.get_number_of_passengers(), self.__cargo_hold)
+
+    def calculate_amount_of_fuel(self, km):
+        fuel_consumption = 0.25 * km * self.get_number_of_passengers() + 2 * km * self.__cargo_hold
+        return fuel_consumption
+
+
+class ShortHaulAircraft(Aircraft):
+
+    counter = 0
+
+    def __init__(self, number_of_passengers, name):
+        super().__init__(number_of_passengers, name)
+        self.__serial_number = ShortHaulAircraft.counter
+        ShortHaulAircraft.counter += 1
+        self.manifest = "Short haul flight serial number {}, name {}: passenger count {}".format(self.__serial_number, self.get_name(), self.get_number_of_passengers())
+
+    def calculate_amount_of_fuel(self, km):
+        return 0.1 * km * self.get_number_of_passengers()
+
+    def get_serial_number(self):
+        return self.__serial_number
+
+
+class ControlTower:
+
+    def __init__(self):
+        self.__aircrafts = []
+
+    def add_aircraft(self, aircraft):
+        self.__aircrafts.append(aircraft)
+
+    def get_manifests(self):
+        manifests = []
+        for aircraft in self.__aircrafts:
+            manifests.append(aircraft.manifest)
+
+        return manifests
+
+
+# ======================================================================================================================
+
 if __name__ == '__main__':
     intercontinental_flight = IntercontinentalAircraft(500, "Boeing-747", 100)
     short_haul_flight = ShortHaulAircraft(110, "Airbus-A220")
--- Informatik/E8/task_2.py
+++ Informatik/E8/task_2.py
@@ -1,3 +1,108 @@
+# ========= Your classes ==========
+class UniPerson:
+
+    def __init__(self, name):
+        self._name = name
+        self.__inbox = []
+
+    def receive_email(self, text):
+        self.__inbox.append(text)
+
+    def read_emails(self):
+        emails = []
+        for email in self.__inbox:
+            emails.append(email)
+
+        self.__inbox.clear()
+        return emails
+
+    def __str__(self):
+        return "Name: {}".format(self._name)
+
+
+class Student(UniPerson):
+
+    students_per_year = {}
+
+    def __init__(self, name, start_year, has_graduated, ects):
+        super().__init__(name)
+        self.start_year = start_year
+        self.has_graduated = has_graduated
+        self.__ects = ects
+        number = Student.get_update_students_per_year(self, start_year)
+        self.__legi_nr = "{}-{}".format(self.start_year, str(number).zfill(5))
+
+    def __str__(self):
+        name = super(Student, self).__str__()
+        return "{}, Legi-Nr.: {}, Graduated: {}, Ects: {}".format(name, self.__legi_nr,
+                                                                  self.has_graduated, self.__ects)
+
+    def receive_email(self, text):
+        super().receive_email(text)
+
+    def read_emails(self):
+        return super().read_emails()
+
+    @staticmethod
+    def get_update_students_per_year(self, year):
+        if year in Student.students_per_year:
+            Student.students_per_year[year] += 1
+
+        else:
+            Student.students_per_year[year] = 0
+
+        return Student.students_per_year[year]
+
+
+class Lecturer(UniPerson):
+
+    def __init__(self, name, lecture_name):
+        super().__init__(name)
+        self.__lecture_name = lecture_name
+
+    def __str__(self):
+        return super().__str__() + ": " + self.__lecture_name
+
+    def receive_email(self, text):
+        super().receive_email(text)
+
+    def read_emails(self):
+        return super().read_emails()
+
+
+class UniManagement:
+
+    def __init__(self):
+        self.__persons = []
+
+    def add_person(self, person):
+        if isinstance(person, UniPerson):
+            self.__persons.append(person)
+
+    def list_persons(self):
+        persons = []
+        for person in self.__persons:
+            persons.append(person.__str__())
+
+        return persons
+
+    def send_email(self, text):
+        for person in self.__persons:
+            person.receive_email(text)
+
+    def count_alumni(self):
+        alumni_counter = 0
+        for person in self.__persons:
+            if isinstance(person, Student):
+                if person.has_graduated:
+                    alumni_counter += 1
+
+        return alumni_counter
+
+
+# ======================================================================================================================
+
+
 if __name__ == '__main__':
     p1 = UniPerson("Hans Muster")
     assert p1.__str__() == "Name: Hans Muster"
